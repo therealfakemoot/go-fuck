8cfe142b09aaa326c2f4ca6de4d48a68d25ae790
Nick Dumas <dumasn@homes.com>
2020-02-13 13:25:59 -0500

    Tokens get a little more complicated.
    Tokens need to carry jump index data. Possibly.

--- tokens.go
+++ tokens.go
@@ -3,15 +3,22 @@ package main
 // type stateFunc func(m *Memory)
 type stateFunc func(*lexer) stateFunc
 
-type Token string
+type Token struct {
+	Raw  string
+	Jump int // Index of corresponding jump
+}
 
-const (
-	RShiftToken Token = ">"
-	LShiftToken Token = "<"
-	IncToken    Token = "+"
-	DecToken    Token = "-"
-	GetToken    Token = "."
-	SetToken    Token = ","
-	LJumpToken  Token = "["
-	RJumpToken  Token = "]"
+func (t Token) String() string {
+	return t.Raw
+}
+
+var (
+	RShiftToken = Token{Raw: ">"}
+	LShiftToken = Token{Raw: "<"}
+	IncToken    = Token{Raw: "+"}
+	DecToken    = Token{Raw: "-"}
+	GetToken    = Token{Raw: "."}
+	SetToken    = Token{Raw: ","}
+	LJumpToken  = Token{Raw: "["}
+	RJumpToken  = Token{Raw: "]"}
 )
